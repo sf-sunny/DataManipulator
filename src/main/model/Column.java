09bde517714189238656d44bb10451e57187e9ba
Sunny Lau <lausunny@students.cs.ubc.ca>
2021-10-12 20:18:10 -0700

    updates on implementation and tests on Column

--- src/main/model/Column.java
+++ src/main/model/Column.java
@@ -6,50 +6,214 @@ import java.util.List;
 public class Column {
     private List<Object> column;
     private String name;
+    private String type;
+    //type can be "i" = Integer, "d" = Double, "s" = String, "o" = Object(unspecified/default)
+    private Integer size;
 
     public Column() {
         column = new LinkedList<>();
+        name = "";
+        type = "o";
+        size = 0;
     }
 
-    //REQUIRES: valid datatype initials
+    //REQUIRES: valid datatype initials ("i" = Integer, "d" = Double, "s" = String, "o" = Object),
+    //          and the datatype should match the actual data in Column
+    //          e.g. this function will not function properly
+    //          while specifying "i" over a column full of characters and strings
+    //          e.g.2. if object 1.1 is casted as integer, then the element will be changed to 1
     //MODIFIES: this
     //EFFECTS: change the actual type of all elements in column
-    public void specify(String colType) {
-
+    public void specifyType(String colType) {
+        if (colType == "i") {
+            type = "i";
+            for (Object o : column) {
+                //System.out.println(o.getClass().getName());
+                String s = o.toString();
+                o = (int) Double.parseDouble(s);
+            }
+        } else if (colType == "d") {
+            type = "d";
+            for (Object o : column) {
+                String s = o.toString();
+                o = Double.valueOf(s);
+            }
+        } else if (colType == "s") {
+            type = "s";
+            for (Object o : column) {
+                o = o.toString();
+            }
+        } else if (colType == "o") {
+            type = "o";
+        }
     }
 
-    //REQUIRES: this.column and another column has same length, and they are addable,
+    //REQUIRES: a valid string
+    //MODIFIES: this
+    //EFFECTS: change the name of a Column
+    public void renameCol(String newName) {
+        name = newName;
+    }
+
+
+    //REQUIRES: a valid list with the supertype of elements in List is Object
+    //MODIFIES: this
+    //EFFECTS: assign a list to this.column
+    public void listAsCol(List<Object> list) {
+        List<Object> newList = new LinkedList<>();
+        newList.addAll(list);
+        column = newList;
+        size = list.size();
+    }
+
+
+    //REQUIRES: a list whose elements have same type as elements in this.column
+    //MODIFIES: this
+    //EFFECTS: append this.column with col, with all elements casted as column.type
+    // type?????
+    public void appendCol(Column col) {
+        column.addAll(col.getCol());
+        this.specifyType(type);
+        size += col.getSize();
+    }
+
+
+    //REQUIRES: this.column and another column has same length, and they are addable (either Integer or Double),
     //             and resultant sum of each element are within the range of their data type
     //EFFECTS: return a Column with sum of two columns
-    public Column add() {
-        return new Column();
+    public Column add(Column another) {
+        List<Object> anotherList = another.getCol();
+        List<Object> finalList = new LinkedList<>();
+        Boolean bothInt;
+
+        if ((another.getType() == "i") && (type == "i")) {
+            bothInt = true;
+        } else {
+            bothInt = false;
+        }
+
+        int i = 0;
+        for (Object o:column) {
+            if (bothInt) {
+                finalList.add(Integer.parseInt(o.toString()) + Integer.parseInt(anotherList.get(i).toString()));
+            } else {
+                finalList.add(Double.parseDouble(o.toString()) + Double.parseDouble(anotherList.get(i).toString()));
+            }
+            i++;
+        }
+
+        Column finalCol = new Column();
+        finalCol.listAsCol(finalList);
+        return finalCol;
     }
 
-    //REQUIRES: this.column and another column has same length, and they are subtractable,
+    //REQUIRES: this.column and another column has same length, and they are subtractable (either Integer or Double),
     //            and resultant difference of each element are within the range of their data type
-    //EFFECTS: return a Column with difference of two columns
-    public Column minus() {
-        return new Column();
+    //EFFECTS: return a Column with elements of this.column - elements of another.column
+    public Column minus(Column another) {
+        List<Object> anotherList = another.getCol();
+        List<Object> finalList = new LinkedList<>();
+        Boolean bothInt;
+
+        if ((another.getType() == "i") && (type == "i")) {
+            bothInt = true;
+        } else {
+            bothInt = false;
+        }
+
+        int i = 0;
+        for (Object o:column) {
+            if (bothInt) {
+                finalList.add(Integer.parseInt(o.toString()) - Integer.parseInt(anotherList.get(i).toString()));
+            } else {
+                finalList.add(Double.parseDouble(o.toString()) - Double.parseDouble(anotherList.get(i).toString()));
+            }
+            i++;
+        }
+
+        Column finalCol = new Column();
+        finalCol.listAsCol(finalList);
+        return finalCol;
     }
 
-    //REQUIRES: this.column and another column has same length, and they are multipliable,
+    //REQUIRES: this.column and another column has same length, and they are multipliable (either Integer or Double),
     //            and resultant product of each element are within the range of their data type
     //EFFECTS: return a Column with product of two columns
-    public Column multiply() {
-        return new Column();
+    public Column multiply(Column another) {
+        List<Object> anotherList = another.getCol();
+        List<Object> finalList = new LinkedList<>();
+        Boolean bothInt;
+
+        if ((another.getType() == "i") && (type == "i")) {
+            bothInt = true;
+        } else {
+            bothInt = false;
+        }
+
+        int i = 0;
+        for (Object o:column) {
+            if (bothInt) {
+                finalList.add(Integer.parseInt(o.toString()) * Integer.parseInt(anotherList.get(i).toString()));
+            } else {
+                finalList.add(Double.parseDouble(o.toString()) * Double.parseDouble(anotherList.get(i).toString()));
+            }
+            i++;
+        }
+
+        Column finalCol = new Column();
+        finalCol.listAsCol(finalList);
+        return finalCol;
     }
 
-    //REQUIRES: this.column and another column has same length, and they are divisible,
-    //            and resultant quotient of elements of this.column divided by elements of another column
-    //            are within the range of double
+    //REQUIRES: this.column and another column has same length, and they are divisible (either Integer or Double),
+    //            resultant quotient of elements of this.column divided by elements of another column,
+    //            results are within the range of double,
+    //            and another column should not contain any 0 element
     //EFFECTS: return a Column with quotients, stored as Double,
-    //          of elements of this.column divided by elements of another column
-    public Column divideDouble() {
-        return new Column();
+    //          of elements of this.column divided by elements of another column.
+    public Column divideDouble(Column another) {
+        List<Object> anotherList = another.getCol();
+        List<Object> finalList = new LinkedList<>();
+
+        int i = 0;
+        for (Object o:column) {
+                finalList.add(Double.parseDouble(o.toString()) / Double.parseDouble(anotherList.get(i).toString()));
+            i++;
+        }
+
+        Column finalCol = new Column();
+        finalCol.listAsCol(finalList);
+        return finalCol;
     }
 
     //EFFECTS: return name of column
     public String getName() {
         return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public Object get(Integer index) {
+        Object o = column.get(index);
+        return o;
+    }
+
+    public List<Object> getCol() {
+        List<Object> l = new LinkedList<>();
+        l.addAll(column);
+        return l;
+    }
+
+
+    //EFFECTS: return True if actual type of elements of column is Integer or Double,
+    //          return False otherwise.
+    public Boolean checkIfArithmetic() {
+        return ((type == "i") || (type == "d"));
     }
 }
--- src/test/model/ColumnTest.java
+++ src/test/model/ColumnTest.java
@@ -1,61 +1,306 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ColumnTest {
+    Column colI, colD, colS;
+    List<Object> listI, listD, listS;
+    @BeforeEach
+    void ini(){
+        colI = new Column();
+        colD = new Column();
+        colS = new Column();
+        listI = new LinkedList<>();
+        listI.add(-7);
+        listI.add(0);
+        listI.add(9);
+        listI.add(79);
+        listD = new LinkedList<>();
+        listD.add(0);
+        listD.add(-9.7861);
+        listD.add(1.234);
+        listD.add(279.436);
+        listS = new LinkedList<>();
+        listS.add("A");
+        listS.add("b");
+        listS.add("Cc");
+        listS.add("Dsef");
+        colI.listAsCol(listI);
+        colD.listAsCol(listD);
+        colS.listAsCol(listS);
+        colI.specifyType("i");
+        colD.specifyType("d");
+        colS.specifyType("s");
+    }
+
     //specify
     @Test
-    void specifyTest() {
+    void specifyTypeTest() {
+        colI.specifyType("i");
+        assertEquals(colI.getType(), "i");
+        colI.specifyType("o");
+        assertEquals(colI.getType(), "o");
+        colI.specifyType("s");
+        assertEquals(colI.getType(), "s");
+        colI.specifyType("d");
+        assertEquals(colI.getType(), "d");
+        colI.specifyType("i");
+        assertEquals(colI.getType(), "i");
+        colD.specifyType("i");
+        assertEquals(colD.getType(), "i");
+        colS.specifyType("s");
+        assertEquals(colS.getType(), "s");
+    }
 
+    //name
+    @Test
+    void renameColTest() {
+        colI.renameCol("hi");
+        assertEquals(colI.getName(), "hi");
+    }
+
+    //list
+    @Test
+    void listAsColTest(){
+        Column colI2 = new Column();
+        colI2.listAsCol(listI);
+        assertEquals(colI2.getSize(), listI.size());
+    }
+
+    @Test
+    void appendColTest(){
+        List<Object> list1 = new LinkedList<>();
+        list1.add(9);
+        list1.add(-84);
+        int oldSize = colI.getSize();
+
+        Column col1 = new Column();
+        col1.listAsCol(list1);
+        colI.appendCol(col1);
+        int newSize = oldSize + list1.size();
+        assertEquals(colI.getSize(), newSize);
+
+        Column col2 = new Column();
+        col2.appendCol(col1);
+        assertEquals(col2.getSize(), list1.size());
     }
 
     //add
     @Test
     void addTestSame() {
+        List<Object> list2 = new LinkedList<>();
+        list2.add(-1); list2.add(-39); list2.add(-7); list2.add(97);
 
+        Column col2 = new Column();
+        col2.listAsCol(list2);
+        col2.specifyType("i");
+        colI.specifyType("i");
+
+        Column sum = col2.add(colI);
+        assertEquals(sum.get(0), -7+-1);
+        assertEquals(sum.get(1), 0-39);
+        assertEquals(sum.get(2), 9-7);
+        assertEquals(sum.get(3), 79+97);
+
+        List<Object> list3 = new LinkedList<>();
+        list3.add(-1.0); list3.add(-39.8); list3.add(-7.14); list3.add(97.5);
+
+        Column col3 = new Column();
+        col3.listAsCol(list3);
+
+        sum = col3.add(colD);
+        assertTrue( checkPrecision((double) sum.get(0), (-1.0+0)  ));
+        assertTrue( checkPrecision((double) sum.get(1), (-39.8+-9.7861) ));
+        assertTrue( checkPrecision((double) sum.get(2), (-7.14+1.234)  ));
+        assertTrue( checkPrecision((double) sum.get(3), (97.5+279.436) ));
     }
 
     @Test
     void addTestDiff() {
+        List<Object> list2 = new LinkedList<>();
+        list2.add(-1.0); list2.add(-39.8); list2.add(-7.14); list2.add(97.5);
 
+        Column col2 = new Column();
+        col2.listAsCol(list2);
+
+        Column sum = col2.add(colI);
+        assertTrue( checkPrecision((double) sum.get(0), (-1.0+-7)  ));
+        assertTrue( checkPrecision((double) sum.get(1), (-39.8+0) ));
+        assertTrue( checkPrecision((double) sum.get(2), (-7.14+9)  ));
+        assertTrue( checkPrecision((double) sum.get(3), (97.5+79) ));
     }
 
     //minus
     @Test
     void minusTestSame() {
+        List<Object> list2 = new LinkedList<>();
+        list2.add(-1); list2.add(-39); list2.add(-7); list2.add(97);
 
+        Column col2 = new Column();
+        col2.listAsCol(list2);
+        col2.specifyType("i");
+
+        Column sum = col2.minus(colI);
+        assertEquals(sum.get(0), -1+7);
+        assertEquals(sum.get(1), -39-0);
+        assertEquals(sum.get(2), -7-9);
+        assertEquals(sum.get(3), 97-79);
+
+        List<Object> list3 = new LinkedList<>();
+        list3.add(-1.0); list3.add(-39.8); list3.add(-7.14); list3.add(97.5);
+
+        Column col3 = new Column();
+        col3.listAsCol(list3);
+        col3.specifyType("d");
+
+        sum = col3.minus(colD);
+        assertTrue( checkPrecision((double) sum.get(0), (-1.0-0)  ));
+        assertTrue( checkPrecision((double) sum.get(1), (-39.8+9.7861) ));
+        assertTrue( checkPrecision((double) sum.get(2), (-7.14-1.234)  ));
+        assertTrue( checkPrecision((double) sum.get(3), (97.5-279.436) ));
     }
 
     @Test
     void minusTestDiff() {
+        List<Object> list2 = new LinkedList<>();
+        list2.add(-1.0); list2.add(-39.8); list2.add(-7.14); list2.add(97.5);
 
+        Column col2 = new Column();
+        col2.listAsCol(list2);
+
+        Column sum = col2.minus(colI);
+        assertTrue( checkPrecision((double) sum.get(0), (-1.0+7)  ));
+        assertTrue( checkPrecision((double) sum.get(1), (-39.8-0) ));
+        assertTrue( checkPrecision((double) sum.get(2), (-7.14-9)  ));
+        assertTrue( checkPrecision((double) sum.get(3), (97.5-79) ));
     }
 
     //multiply
     @Test
     void multiplyTestSame() {
+        List<Object> list2 = new LinkedList<>();
+        list2.add(-1); list2.add(-39); list2.add(-7); list2.add(97);
 
+        Column col2 = new Column();
+        col2.listAsCol(list2);
+        col2.specifyType("i");
+
+        Column sum = col2.multiply(colI);
+        assertEquals(sum.get(0), -1*-7);
+        assertEquals(sum.get(1), -39*0);
+        assertEquals(sum.get(2), -7*9);
+        assertEquals(sum.get(3), 97*79);
+
+        List<Object> list3 = new LinkedList<>();
+        list3.add(-1.0); list3.add(-39.8); list3.add(-7.14); list3.add(97.5);
+
+        Column col3 = new Column();
+        col3.listAsCol(list3);
+        col3.specifyType("d");
+
+        sum = col3.multiply(colD);
+        assertTrue( checkPrecision((double) sum.get(1), (-39.8*-9.7861)  ));
+        assertTrue( checkPrecision((double) sum.get(2), (-7.14*1.234)   ));
+        assertTrue( checkPrecision((double) sum.get(3), (97.5*279.436) ));
+        assertTrue(((-1.0*0)< 0.00001) && ((double) sum.get(0) < 0.00001));
     }
 
     @Test
     void multiplyTestDiff() {
+        List<Object> list2 = new LinkedList<>();
+        list2.add(-1.0); list2.add(-39.8); list2.add(-7.14); list2.add(97.5);
 
+        Column col2 = new Column();
+        col2.listAsCol(list2);
+
+        Column sum = col2.multiply(colI);
+
+        assertTrue( checkPrecision((double) sum.get(0), (-1.0*-7) ));
+        assertTrue( checkPrecision((double) sum.get(2), (-7.14*9)  ));
+        assertTrue( checkPrecision((double) sum.get(3), (97.5*79)  ));
+        assertTrue(((-39.8*0)< 0.00001) && (double) sum.get(1) < 0.00001);
     }
 
+    boolean checkPrecision(double a, double b) {
+        return (Math.abs(a-b) / a < 0.00001);
+    }
     //divide
     @Test
     void divideDoubleTestSame() {
+        List<Object> list2 = new LinkedList<>();
+        list2.add(-1); list2.add(-39); list2.add(-7); list2.add(97);
 
+        Column col2 = new Column();
+        col2.listAsCol(list2);
+        col2.specifyType("i");
+        colI.specifyType("i");
+
+        Column sum = col2.divideDouble(colI);
+        assertTrue( checkPrecision((double) sum.get(0), (double) -1/-7) );
+        assertTrue( checkPrecision((double) sum.get(2), (double) -7/9) );
+        assertTrue( checkPrecision((double) sum.get(3), (double) 97/79) );
+
+
+        List<Object> list3 = new LinkedList<>();
+        list3.add(-1.0); list3.add(-39.8); list3.add(-7.14); list3.add(97.5);
+
+        Column col3 = new Column();
+        col3.listAsCol(list3);
+
+        sum = col3.divideDouble(colD);
+        assertTrue( checkPrecision((double) sum.get(1), (-39.8/-9.7861) ));
+        assertTrue( checkPrecision((double) sum.get(2), (-7.14/1.234) ));
+        assertTrue( checkPrecision((double) sum.get(3), (97.5/279.436) ));
     }
 
     @Test
     void divideDoubleTestDiff() {
+        List<Object> list2 = new LinkedList<>();
+        list2.add(-1.0); list2.add(-39.8); list2.add(-7.14); list2.add(97.5);
 
+        Column col2 = new Column();
+        col2.listAsCol(list2);
+
+        Column sum = col2.divideDouble(colI);
+        assertTrue( checkPrecision((double) sum.get(0), (-1.0/-7) ));
+        assertTrue( checkPrecision((double) sum.get(2), (-7.14/9)  ));
+        assertTrue( checkPrecision((double) sum.get(3), (97.5/79)  ));
+
+        sum = colI.divideDouble(col2);
+        assertTrue( checkPrecision((double) sum.get(0), 1/(-1.0/-7) ));
+        assertTrue( checkPrecision((double) sum.get(2), 1/(-7.14/9)  ));
+        assertTrue( checkPrecision((double) sum.get(3), 1/(97.5/79)  ));
     }
 
     //getName
     @Test
     void getNameTest() {
+        colI.renameCol("abcdfnde");
+        assertTrue(colI.getName() == "abcdfnde");
+        assertTrue(colD.getName() == "");
+    }
 
+    @Test
+    void getTypeTest() {
+        assertTrue(colI.getType() == "i");
+        assertTrue(colD.getType() == "d");
+        assertTrue(colS.getType() == "s");
+        Column col1 = new Column();
+        assertTrue(col1.getType() == "o");
+    }
+
+    @Test
+    void checkIfArithmeticTest() {
+        assertTrue(colI.checkIfArithmetic());
+        assertTrue(colD.checkIfArithmetic());
+        assertFalse(colS.checkIfArithmetic());
+        Column col1 = new Column();
+        assertFalse(col1.checkIfArithmetic());
     }
 }
