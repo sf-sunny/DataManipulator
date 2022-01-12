b8146afd0ce8e28cae43bbe58dd7289aba8be69d
sf-sunny <sflau.sunny@gmail.com>
2022-01-11 19:37:57 -0800

    test

--- src/main/model/Column.java
+++ src/main/model/Column.java
@@ -8,7 +8,7 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import persistence.Writable;
-
+//test
 // Represents a column from a .csv file
 public class Column implements Writable {
     private List<Object> column;
